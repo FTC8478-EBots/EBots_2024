6afdc03f9144ed644d6566d07103aafdbe1d812a
ebots8478 <146142210+ebots8478@users.noreply.github.com>
2024-09-29 12:38:40 -0400

    Update Motor Directions for MecanumDrive and add function prototypes for Elevator and Claw.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Claw.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.components;
 
 public class Claw {
+void open() {
+    throw new RuntimeException("Unimplemented");
+}
+void close() {
+    throw new RuntimeException("Unimplemented");
+}
+void init() {
+    throw new RuntimeException("Unimplemented");
+}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Elevator.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.components;
 
 public class Elevator {
+    void getPosition() {
+        throw new RuntimeException("Unimplemented");
+    }
+    void setPosition() {
+        throw new RuntimeException("Unimplemented");
+    }
+    void init() {
+        throw new RuntimeException("Unimplemented");
+    }
+    void zero() {
+        throw new RuntimeException("Unimplemented");
+    }
 }
