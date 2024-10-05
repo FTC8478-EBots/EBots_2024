c968e26fb20580a9233207c9122ef2111d18adf9
ebots8478 <146142210+ebots8478@users.noreply.github.com>
2024-10-05 08:43:44 -0400

    Initial implementation of Elevator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Elevator.java
@@ -1,16 +1,44 @@
 package org.firstinspires.ftc.teamcode.components;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 public class Elevator {
-    void getPosition() {
+
+    private LinearOpMode opMode;
+    HardwareMap hardwareMap;
+    private DcMotorEx elevatorMotor;
+    private     DigitalChannel zeroLimitSwitch;
+    private boolean isZeroed = false;
+
+    int getPosition() {
+        return 0;
+
+        //throw new RuntimeException("Unimplemented");
+    }
+    void setPosition(int position) {
         throw new RuntimeException("Unimplemented");
     }
-    void setPosition() {
-        throw new RuntimeException("Unimplemented");
+    private void updateTelemetry() {
+        telemetry.addData("Elevator Position", elevatorMotor.getCurrentPosition());
+        telemetry.addData("ZeroSensor", zeroLimitSwitch.getState());
     }
-    void init() {
-        throw new RuntimeException("Unimplemented");
+    void init(LinearOpMode opMode) {
+        telemetry = opMode.telemetry;
+        this.opMode = opMode;
+        this.hardwareMap = this.opMode.hardwareMap;
+        isZeroed = false;
+        this.elevatorMotor = this.hardwareMap.get(DcMotorEx.class, "elevatorMotor");
+        this.zeroLimitSwitch = this.hardwareMap.get(DigitalChannel.class, "elevatorZero");
+        updateTelemetry();
+        telemetry.update();
+        elevatorMotor.setTargetPosition(0);
     }
-    void zero() {
+    void zeroElevator() {
         throw new RuntimeException("Unimplemented");
+
     }
+
 }
