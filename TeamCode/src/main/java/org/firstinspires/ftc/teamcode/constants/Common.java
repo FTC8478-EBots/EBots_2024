d5518d2ff567c52665c5917735f9f24b6aa7ad7e
RoboticsJ <RoboticsJ@users.noreply.github.com>
2024-10-10 15:49:39 -0400

    Add Rotation function.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/Common.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/Common.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.constants;
 
+import com.acmerobotics.roadrunner.Vector2d;
+
 public class Common {
+
+    public static Vector2d Rotate(Vector2d vector2d, double n)
+    {
+        double rx = (vector2d.x * Math.cos(n)) - (vector2d.y * Math.sin(n));
+        double ry = (vector2d.x * Math.sin(n)) + (vector2d.y * Math.cos(n));
+        return new Vector2d(rx,ry);
+    }
 }
